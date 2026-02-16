acb0fe70d90ab687d0e5032007d910271a5de1a2
Penugonda Mani kumar <penugondamanikumar99@gmail.com>
2026-02-16 22:43:54 +0530

    Added environment database config

--- app/database.py
+++ app/database.py
@@ -3,16 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Get DATABASE_URL from environment
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-# If not found (local development), use MySQL
-if not DATABASE_URL:
-    DATABASE_URL = "mysql+pymysql://root:Mani%401234@localhost/student_management"
-
-# Fix for Render postgres URL
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 
